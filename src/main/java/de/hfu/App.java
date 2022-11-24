7694965c1f8fcb794f014a4cc7fb40b16840df25
aarons20 <r.account1@web.de>
2022-11-24 12:38:19 +0100

    Added JavaDoc to App.java

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
  */
 public class App 
 {
+	/**
+	 * 
+	 * @param args
+	 */
     public static void main( String[] args )
     {
     	Scanner scan = new Scanner(System.in);
